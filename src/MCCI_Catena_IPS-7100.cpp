7165991bdc16b4f17e2446e447cfe68e7962e6ac
Pranau-R <pranau@mcci.com>
2022-10-11 12:13:35 +0530

    WIP: Initial Commit

---
+++ src/MCCI_Catena_IPS-7100.cpp
@@ -0,0 +1,18 @@
+/*
+
+Module: MCCI_Catena_IPS-7100.cpp
+
+Function:
+    Implementation code for MCCI Catena IPS-7100 sensor library.
+
+Copyright and License:
+    See accompanying LICENSE file for copyright and license information.
+
+Author:
+    Pranau R, MCCI Corporation   October 2022
+
+*/
+
+#include "MCCI_Catena_IPS-7100.h"
+
+/**** end of MCCI_Catena_IPS-7100.cpp ****/
---
+++ src/MCCI_Catena_IPS-7100.h
@@ -0,0 +1,23 @@
+/*
+
+Module: MCCI_Catena_IPS-7100.h
+
+Function:
+    Top-level include file for MCCI Catena IPS-7100 library.
+
+Copyright and License:
+    See accompanying LICENSE file for copyright and license information.
+
+Author:
+    Pranau R, MCCI Corporation   October 2022
+
+*/
+
+/// \file
+
+#ifndef _MCCI_CATENA_IPS_7100_H_
+#define _MCCI_CATENA_IPS_7100_H_ /* prevent multiple includes */
+
+#pragma once
+
+#endif /* _MCCI_CATENA_IPS_7100_H_ */
