b9322d2884c2f3a9a4b469e0ef9261ffa2f3cb92
Pranau-R <pranau@mcci.com>
2022-10-21 20:54:37 +0530

    #1: successfully compiled

---
+++ examples/compile-test/compile_test.ino
@@ -0,0 +1,26 @@
+/*
+
+Module: compile_test.ino
+
+Function:
+    Compile test for MCCI Catena IPS-7100 sensor library.
+
+Copyright and License:
+    See accompanying LICENSE file for copyright and license information.
+
+Author:
+    Pranau R, MCCI Corporation   October 2022
+
+*/
+
+#include "MCCI_Catena_IPS-7100.h"
+
+void setup()
+        {
+        }
+
+void loop()
+        {
+        }
+
+/**** end of compile_test.ino ****/
